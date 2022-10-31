89a7dc1ca66e487dd1249cd411480e39a700cd6c
oluwafemi13 <sundayjabikem@gmail.com>
2022-10-31 08:38:42 +0100

    Add project files.

---
+++ Controllers/DescriptionController.cs
@@ -0,0 +1,23 @@
+﻿using HNGStage1Task;
+using HNGStage1Task.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNGStage1Task.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DescriptionController : ControllerBase
+    {
+        private static TaskDescription desc = new TaskDescription();
+        
+            
+
+        
+
+        [HttpGet(Name = "GetDescription")]
+        public IActionResult Get()
+        {
+            return Ok(desc);
+        }
+    }
+}
---
+++ Model/TaskDescription.cs
@@ -0,0 +1,13 @@
+﻿namespace HNGStage1Task.Model
+{
+    public class TaskDescription
+    {
+        public string SlackUsername { get; set; } = "Iwuchukwu Ikechukwu";
+
+        public bool Backend { get; set; } = true;
+
+        public int Age { get; set; } = 25;
+
+        public string Bio { get; set; } = "Software Engineer";
+    }
+}
